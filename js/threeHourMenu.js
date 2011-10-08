b7eb8cb5037db5852eb99986fe8df16e523c0be6
Shaun O'Connell <shaun@tactile.co.za>
2011-10-08 15:02:33 +0200

    Now getting JSON menu data asynchronously
    Added XMLHTTPRequest to get the JSON Data in an async fashion.
    Next up, spitting out the root elements based on the menu data

--- js/threeHourMenu.js
+++ js/threeHourMenu.js
@@ -1,11 +1,18 @@
 var menu = {
+	data: null,
 	get_menu_data: function(){
 		// open XHR and load the JSON
 		console.log("menu.get_menu_data called");
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "../json/menu.json", false);
+		xhr.send(null);
+		menu.data = eval("(" + xhr.responseText + ")"); // This will need to be parsed for bad characters, HTML, etc.
 	},
 	init: function(){
 		// Set configuration, Get menu data, etc
 		console.log("menu.init called");
+		menu.get_menu_data();
+		console.log(menu.data);
 	}
 	
 }
